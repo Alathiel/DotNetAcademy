39299e570b4ced384fd514c8975465fefd5c855b
Federico Pappalardo <federico.pappalardo2403@gmail.com>
2024-03-21 17:11:07 +0100

    Added project

---
+++ Mezzi/DataModels/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mezzi.DataModels
+{
+    internal class Enums
+    {
+        internal enum Type{ 
+            Bicicletta,
+            Automobile,
+            Aereo,
+            Monopattino,
+            Nave,
+        }
+
+        internal enum Color
+        {
+            Red,
+            Green,
+            Blue,
+            Purple,
+            Black,
+            White,
+        }
+    }
+}
---
+++ Mezzi/DataModels/Mezzo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mezzi.DataModels
+{
+    public abstract class Mezzo
+    {
+        public abstract int PeopleCapacity { get; set; }
+        public abstract bool Electric { get; set; }
+        public abstract bool Engine { get; set; }
+        public abstract Enum Type { get; set; }
+        public abstract Enum Color { get; set; }
+        public abstract bool License { get; set; }
+    }
+
+    public interface IBike
+    {
+        public void ShowInfos();
+        
+        
+    }
+}
